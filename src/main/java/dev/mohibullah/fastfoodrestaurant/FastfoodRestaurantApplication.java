92f2006fe3e9fa322dd3a1855460010a478ce27f
S.M. Mohibullah Bukhari <mohibullahbukhari98@gmail.com>
2023-09-03 13:03:28 +0500

    Initial Commit

---
+++ src/main/java/dev/mohibullah/fastfoodrestaurant/FastfoodRestaurantApplication.java
@@ -0,0 +1,13 @@
+package dev.mohibullah.fastfoodrestaurant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FastfoodRestaurantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FastfoodRestaurantApplication.class, args);
+	}
+
+}
